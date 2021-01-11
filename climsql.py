f0eee09e6517f465f0cb31fdee0a017a1b91d04f
EliteLordy <62451504+el1telordy@users.noreply.github.com>
2021-01-11 17:28:56 +0300

    Fixed "cached values" bug

--- climsql.py
+++ climsql.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 
-import sys, adafruit_dht, pytz
+import adafruit_dht, time, mysql.connector
 from datetime import datetime
-import mysql.connector
 
-PIN=4
+PIN=4 #DATA GPIO PIN
 device = adafruit_dht.DHT22(PIN)
 
 cnx = mysql.connector.connect(user='pi', password='xiaomitop', database='climate')
 cursor = cnx.cursor(buffered=True)
 
 def getTime():
-    # current date and time
-    localTz = pytz.timezone('Europe/Moscow')
+    #current date
     now = datetime.now()
     timestamp = datetime.timestamp(now)
     return(datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
 
 def getSensors():
     temp = int(device.temperature)
-    #temp = 25
-    #hum = 32
     hum = int(device.humidity)
     return(temp, hum)
 
 def main():
     time = getTime()
     print(time)
+    getSensors() #"warm-up" function call
+    time.sleep(2)
     t = getSensors()[0]
     h = getSensors()[1]
     cursor.execute("""INSERT INTO clim1 (date, temperature, humidity) VALUES ('%s', %s, %s)""" % (time,t,h))
@@ -36,4 +34,4 @@ def main():
     cnx.close()
 
 if __name__ == "__main__":
-   main()
+    main()
